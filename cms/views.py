e347aedb1b0c9f94155ca25be3d1fc2187ef1ee8
Doppon <zaxpkuraudo7777@gmail.com>
2018-08-27 01:22:10 +0700

    ビューの内容を更新

--- cms/views.py
+++ cms/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 
 def book_list(request):
     """書籍の一覧"""
-    return HttpResponse('書籍の一覧')
+    # return HttpResponse('書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render(request,
+                  'cms/book_list.html',     # 使用するテンプレート
+                  {'books': books})         # テンプレートに渡すデータ
 
 
 def book_edit(request, book_id=None):
